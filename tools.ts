2548149542784c49c2d8377bff857ba337db28aa
Mark Dierolf <mdierolf@cloudquote.io>
2025-12-02 17:57:44 -0800

    Refactor imports to use named exports instead of `default` for improved clarity and consistency.

--- tools.ts
+++ tools.ts
@@ -1,8 +1,8 @@
-import {default as getHeadlinesBySecurity} from "./tools/getHeadlinesBySecurity.ts";
-import {default as getLeaders} from "./tools/getLeaders.ts";
-import {default as getPriceHistory} from "./tools/getPriceHistory.ts";
-import {default as getPriceTicks} from "./tools/getPriceTicks.ts";
-import {default as getQuote} from "./tools/getQuote.ts";
+import getHeadlinesBySecurity from "./tools/getHeadlinesBySecurity.ts";
+import getLeaders from "./tools/getLeaders.ts";
+import getPriceHistory from "./tools/getPriceHistory.ts";
+import getPriceTicks from "./tools/getPriceTicks.ts";
+import getQuote from "./tools/getQuote.ts";
 
 export default {
   getQuote,
