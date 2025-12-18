bfbf7adc7c6ffe51a5c4ba2891af1b3cd5c9672d
Mark Dierolf <mdierolf@cloudquote.io>
2025-12-18 13:53:38 -0800

    Extract and modularize plugin logic into `plugin.ts`

---
+++ plugin.ts
@@ -0,0 +1,23 @@
+import TokenRingApp, {TokenRingPlugin} from "@tokenring-ai/app";
+import {ChatService} from "@tokenring-ai/chat";
+import CloudQuoteService, {CloudQuoteServiceOptionsSchema} from "./CloudQuoteService.ts";
+import packageJSON from './package.json' with {type: 'json'};
+import tools from "./tools.ts";
+
+
+
+export default {
+  name: packageJSON.name,
+  version: packageJSON.version,
+  description: packageJSON.description,
+  install(app: TokenRingApp) {
+
+    app.waitForService(ChatService, chatService =>
+      chatService.addTools(packageJSON.name, tools)
+    );
+    const config = app.getConfigSlice('cloudquote', CloudQuoteServiceOptionsSchema.optional());
+    if (config) {
+      app.addServices(new CloudQuoteService(config));
+    }
+  },
+} satisfies TokenRingPlugin;
